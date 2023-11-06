ed39f2aefbbf67793fc4596c2c1c9232a075670d
Ebio Narduche <ebionarduche77@gmail.com>
2023-11-06 20:39:03 -0300

    feat: Adiciona ícones ao cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style/Header.css';
 import profileIcon from '../data/icons/profile-icon.png';
 import cartIcon from '../data/icons/shopping-cart-icon.png';
+import FavIcon from '../data/icons/icons-heart-on.png';
 
 function Header() {
   return (
@@ -11,6 +12,7 @@ function Header() {
       <div className="header-icon-container">
         <img src={ profileIcon } alt="icon profile" width="50px" />
         <img src={ cartIcon } alt="icon cartshopping" width="50px" />
+        <img src={ FavIcon } alt="" width="50px" />
       </div>
     </div>
   );
