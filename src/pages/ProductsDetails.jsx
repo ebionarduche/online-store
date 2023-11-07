1a49048b97e58fe07b5426be6cb755d2009c3887
Ebio Narduche <ebionarduche77@gmail.com>
2023-11-06 21:41:33 -0300

    style: Aplica estilização a pagina de Products Details

--- src/pages/ProductsDetails.jsx
+++ src/pages/ProductsDetails.jsx
@@ -3,29 +3,36 @@ import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import products from '../data/products';
 import ProductReviews from '../components/ProductsDetails/ProductReviews';
+import '../components/ProductsDetails/ProductsDetails.css';
+import cartIcon from '../data/icons/shopping-cart-icon.png';
 
 function ProductsDetails() {
   const { id } = useParams();
   const productToShow = products[id - 1];
   return (
-    <div className="products-details-container">
+    <div>
       <Header />
-      <h1>{productToShow.name}</h1>
-      <h2>{productToShow.characteristics.brand}</h2>
-      <img src={ productToShow.primaryImage } alt="" width="200px" />
-      <h2>
-        Cores disponiveis:
-        {' '}
-        {productToShow.characteristics.cor.join(', ')}
-      </h2>
+      <div className="products-details-container">
+        <h1>{productToShow.name}</h1>
+        <h2>{productToShow.characteristics.brand}</h2>
+        <img src={ productToShow.primaryImage } alt="" width="200px" />
+        <h2>
+          Cores disponiveis:
+          {' '}
+          {productToShow.characteristics.cor.join(', ')}
+        </h2>
 
-      <span>
-        R$
-        {' '}
-        {productToShow.price}
-      </span>
-      <p>{productToShow.description}</p>
+        <span>{`R$ ${productToShow.price}`}</span>
+        <p>{productToShow.description}</p>
 
+        <button type="button">
+          Comprar
+        </button>
+
+        <button type="button">
+          <img src={ cartIcon } alt="" width="10px" />
+        </button>
+      </div>
       <ProductReviews product={ products[id - 1] } />
     </div>
   );
