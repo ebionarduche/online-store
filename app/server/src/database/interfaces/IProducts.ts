76172665e99048d9fa5b49936866fe0226d4fe02
Ebio Narduche <ebionarduche77@gmail.com>
2023-11-23 20:14:33 -0300

    Feat: create table-products

---
+++ app/server/src/database/interfaces/IProducts.ts
@@ -0,0 +1,12 @@
+export interface IProducts {
+  id: number,
+  name: string
+  description: string,
+  categoriesId: number,
+  stock: number,
+  brand: string,
+  salesFormat: string,
+  colors: string,
+  price: number,
+  quantity: number,
+}
---
+++ app/server/src/database/migrations/02-create-products.table.ts
@@ -0,0 +1,60 @@
+import { Model, QueryInterface, DataTypes } from "sequelize";
+import { IProducts } from "../interfaces/IProducts";
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IProducts>>('products', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      categoriesId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        field: 'categories_id'
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      salesFormat: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        field: 'sales_format',
+      },
+      colors: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    })
+  },
+  down(queryInterface: QueryInterface) {
+    return(queryInterface.dropTable('products'))
+  }
+}
