951eb9dfb6dda233649ed31a954e333ce3fdc0e5
Ebio Narduche <ebionarduche77@gmail.com>
2023-12-01 18:37:13 -0300

    feat: Implementação de contador productCard | style: Troca de icons e aplicações de efeitos mouse hover

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory, Link } from 'react-router-dom';
-
 import './style/Header.css';
-import profileIcon from '../data/icons/profile-icon.png';
-import cartIcon from '../data/icons/shopping-cart-icon.png';
-import FavIcon from '../data/icons/icons-heart-on.png';
+
+import { ReactComponent as CartIcon } from 'feather-icons/dist/icons/shopping-cart.svg';
+import { ReactComponent as UserIcon } from 'feather-icons/dist/icons/user.svg';
+import { ReactComponent as HeartIcon } from 'feather-icons/dist/icons/heart.svg';
 import logo from '../data/images/logo.png';
+import CartContext from '../context/CartContext';
 
 function Header() {
   const history = useHistory(); // Inicialize o useHistory
+  const { shoppingCart } = useContext(CartContext);
 
-  const navigateToShoppingCart = () => {
-    history.push('/shopping-cart'); // Use history.push para navegar para a página de carrinho
+  const navigateHeader = (link) => {
+    history.push(link);
   };
-
-  const navigateToFavorites = () => {
-    history.push('/favorites'); // Use history.push para navegar para a página de carrinho
-  };
-
   return (
     <div className="header-container">
       <Link to="/">
         <img src={ logo } alt="icons kabuy" width="220px" />
       </Link>
+
       <input type="text" placeholder="Search..." />
+
       <div className="header-icon-container">
-        <img src={ profileIcon } alt="icon profile" width="50px" />
-        <button onClick={ navigateToFavorites }>
-          <img src={ FavIcon } alt="" width="50px" />
+        <button onClick={ () => navigateHeader('/perfil') }>
+          <UserIcon className="header-iconSGV" />
         </button>
-        <button onClick={ navigateToShoppingCart }>
-          <img src={ cartIcon } alt="icon cartshopping" width="50px" />
+        <button onClick={ () => navigateHeader('/favorites') }>
+          <HeartIcon className="header-iconSGV" />
         </button>
+        <button onClick={ () => navigateHeader('/shopping-cart') }>
+          <CartIcon className="header-iconSGV" />
+          <p className="header-cart-counter">
+            {shoppingCart.length > 0
+              ? Number(shoppingCart[shoppingCart.length - 1].cartLenght)
+              : ''}
+          </p>
+        </button>
+
       </div>
     </div>
   );
