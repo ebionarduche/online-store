50e4eb07b286dc306b2dee8610bb9b4e715513d7
Ebio Narduche <ebionarduche77@gmail.com>
2023-12-01 18:46:23 -0300

    refactor: Aprimorando o Objeto Provider

--- app/client/src/context/CartProvider.js
+++ app/client/src/context/CartProvider.js
@@ -4,14 +4,18 @@ import CartContext from './CartContext';
 
 function CartProvider({ children }) {
   const [shoppingCart, setShoppingCart] = useState([]);
+  const [counter, setCounter] = useState(0);
 
   const InsertProductCart = (product) => {
+    setCounter((prevCounter) => prevCounter + 1);
     const newProduct = {
       id: product.id,
       productName: product.name,
       image: product.primaryImage,
       price: product.price,
       brand: product.characteristics.brand,
+      amount: 1,
+      cartLenght: counter + 1,
     };
     setShoppingCart((prevCart) => [...prevCart, newProduct]);
     console.log(shoppingCart);
